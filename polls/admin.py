19b3e580937b4e3f690371783c3949ee5bbf135b
Asmaa Hermak <asmaa.hermak.social@gmail.com>
2024-10-24 22:01:58 +0100

    customization of admin app

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from .models import Question, Choice
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+            (None, {"fields":["pub_date"]}), 
+            ("Date information",{"fields":["question_text"]}),
+        ]
+    inlines = [ChoiceInline]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+    list_display = ["question_text", "pub_date","was_published_recently"]
+
+
+admin.site.register(Question, QuestionAdmin)
 
 # Register your models here.
--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 import datetime
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -8,12 +10,15 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
-        
+
+    @admin.display(
+        boolean = True,
+        ordering = "pub_date",
+        description = "Published recently?"
+    )  
     def was_published_recently(self):
         now = timezone.now()
         return now >= self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-
 
 
 class Choice(models.Model):
